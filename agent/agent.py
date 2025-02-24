cf0f600f390f95d73aed071a9f2092b2e03cd489
aryan-think <aryan.kamboj@think41@gmail.com>
2025-02-24 22:15:51 +0530

    added the exsiting code

--- agent/agent.py
+++ agent/agent.py
@@ -1,6 +1,8 @@
 import logging
-
+import os
 from dotenv import load_dotenv
+from external_functions.AgentActions import AgentActions
+from external_functions.utility_functions import create_assessment_after_conversation
 from livekit.agents import (
     AutoSubscribe,
     JobContext,
@@ -8,15 +10,15 @@ from livekit.agents import (
     WorkerOptions,
     cli,
     metrics,
+    llm
 )
 from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.plugins import cartesia, deepgram, silero, turn_detector
-from livekit.plugins.openai import llm
+from livekit.plugins import deepgram, silero, turn_detector,openai
 from livekit.agents.llm import ChatContext 
+import json
 
 
-load_dotenv(dotenv_path=".env.local")
-logger = logging.getLogger("voice-agent")
+load_dotenv(dotenv_path=".env")
 
 
 def prewarm(proc: JobProcess):
@@ -24,13 +26,24 @@ def prewarm(proc: JobProcess):
 
 
 async def entrypoint(ctx: JobContext):
-    print("up")
+    metadata = json.loads(participant.metadata)
+    logger.info(f"metadata : {metadata}")
+    logger = logging.getLogger("voice-agent")
+    fnc_ctx = AgentActions()
+    # get the user's info such as current subject and topic selected and store it in the context
+    user_email = ctx.room.name
+    print(user_email)
+    subject  = "maths"
+    topic = "addition"
     initial_ctx = ChatContext().append(
         role="system",
         text=(
-            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
-            "You were created as a demo to showcase the capabilities of LiveKit's agents framework."
+            f"""You are a voice assistant created for helping students with topics they are struggling with. Your interface with users will be voice. 
+            You should use short and concise responses, and avoiding usage of unpronouncable punctuation. 
+            The current subject is maths and the current topic is addition.
+            Strictly provide answers to the question regarding this context only if user asks anything else just say "I can't provide you any information on that".
+            """
+            
         ),
     )
 
@@ -41,14 +54,15 @@ async def entrypoint(ctx: JobContext):
     participant = await ctx.wait_for_participant()
     logger.info(f"starting voice assistant for participant {participant.identity}")
 
-    # This project is configured to use Deepgram STT, OpenAI LLM and Cartesia TTS plugins
-    # Other great providers exist like Cerebras, ElevenLabs, Groq, Play.ht, Rime, and more
-    # Learn more and pick the best one for your app:
-    # https://docs.livekit.io/agents/plugins
+    print("----------------------------------------------metadataaaaaaa----------------------------------")
+    print(participant)
+    print("----------------------------------------------metadataaaaaaa----------------------------------")
+
     agent = VoicePipelineAgent(
+        fnc_ctx=fnc_ctx,
         vad=ctx.proc.userdata["vad"],
         stt=deepgram.STT(),
-        llm = llm.LLM.with_groq(
+        llm = openai.LLM.with_groq(
             model="llama3-8b-8192"
         ),
         tts=deepgram.TTS(
@@ -61,6 +75,7 @@ async def entrypoint(ctx: JobContext):
         max_endpointing_delay=5.0,
         chat_ctx=initial_ctx,
     )
+   
 
     usage_collector = metrics.UsageCollector()
 
@@ -73,6 +88,9 @@ async def entrypoint(ctx: JobContext):
 
     # The agent should be polite and greet the user when it joins :)
     await agent.say("Hey, how can I help you today?", allow_interruptions=True)
+
+    ctx.add_shutdown_callback(create_assessment_after_conversation)
+    # there will be a cleanup funciton that will generate the assignment based on the user's doubts
 
 
 if __name__ == "__main__":
